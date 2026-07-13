2d308a31a3a170f0c9b0f7d5ecb0aa773375db03
CHALLA YOGI <chyogi2006@gmail.com>
2026-07-13 20:02:32 +0530

    Spring Boot Project Setup wit Mysql configuration

---
+++ src/main/java/com/travel/travelhospitality/TravelhospitalityApplication.java
@@ -0,0 +1,13 @@
+package com.travel.travelhospitality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelhospitalityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelhospitalityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travel/travelhospitality/TravelhospitalityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.travelhospitality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelhospitalityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
