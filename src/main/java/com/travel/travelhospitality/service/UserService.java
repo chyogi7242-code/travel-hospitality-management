3a093a11772b110f41839064a42ad03dcc87be7e
CHALLA YOGI <chyogi2006@gmail.com>
2026-07-14 14:41:00 +0530

    Added User module and Spring Boot setup

---
+++ src/main/java/com/travel/travelhospitality/service/UserService.java
@@ -0,0 +1,18 @@
+package com.travel.travelhospitality.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.travel.travelhospitality.entity.User;
+import com.travel.travelhospitality.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
